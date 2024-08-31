fbbaa505e896af20cdf790f130e5b02a89c5cb7d
Erblin Syla <erblin.sylaa@gmail.com>
2024-08-31 22:34:31 +0200

    Deleted all folders and files to start over

--- Level 1/Landing Page/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('hamburger').addEventListener('click', function() {
-    var navLinks = document.querySelector('.nav-links ul');
-    if (navLinks.style.display === "flex") {
-        navLinks.style.display = "none";
-    } else {
-        navLinks.style.display = "flex";
-    }
-});
